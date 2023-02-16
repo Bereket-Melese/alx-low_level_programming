9ac4dc68ef0521917185eba9a4de07be959fc7cb
Bereket-Melese <Bereketmelese3.92@gmail.com>
2023-02-16 11:30:18 -0800

    Hello_world!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+/**
+ *main  -Entry point
+ *return: 0 for success
+ */
 
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
