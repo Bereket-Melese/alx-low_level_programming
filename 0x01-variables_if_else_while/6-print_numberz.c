22e51ea4df29685ac5e4060b3f799c3c9d217e9e
Bereket-Melese <Bereketmelese3.92@gmail.com>
2023-02-17 10:56:39 -0800

    ALX Low-level Programming Task

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: A program that prints all single digit numbers of base 10 starting from 0
+ * Description: A program that prints all single
+ * digit numbers of base 10 starting from 0
  * Return: 0
  */
 int main(void)
 {
-    char c = 0;
+	char c = 0;
 
-    while (c <= 10)
-    {
-        putchar(48 + c);
-        c++;
-    }
-    putchar('\n');
-    return (0);
+	while (c <= 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
 }
