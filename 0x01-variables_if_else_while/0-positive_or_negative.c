0ceafd624cee0797cea609eeb6052a07e584ac14
Bereket-Melese <Bereketmelese3.92@gmail.com>
2023-02-17 10:09:26 -0800

    ALX Low-level Programming Task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,10 +15,9 @@ int main(void)
 
 	if (n > 0)
 		printf("%d is positive\n", n);
-       	else if (n == 0)
+	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%i is negative\n", n);
 	return (0);
 }
-
