0fe15bb7eedea22f24c1ce056379db2d2d2e300a
Bereket-Melese <Bereketmelese3.92@gmail.com>
2023-02-27 11:05:05 -0800

    Pointers, Arrays & Strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,23 +1,17 @@
-#include "main.h"
+#ifndef main
 
-/**
- * *_strcpy - copies the string pointed to by src, including \0.
- *@src: pointer.
- *@dest: pointer.
- *Return: the pointer to dest.
- */
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
-char *_strcpy(char *dest, char *src)
-{
-	int i, length;
-
-	for (length = 0; src[length] != '\0'; length++)
-	{
-	}
-
-	for (i = 0; i <= length ; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (dest);
-}
+#define main
+#endif
